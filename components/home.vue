54fbe28eb396f7336f5492507ee32a2b1ac25a8f
Fatima Turgunbaeva <fturgunbaeva@gmail.com>
2019-04-10 05:44:38 +0600

    Добавлены новые структуры

---
+++ components/home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="spinner-grow text-primary" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div class="spinner-grow text-secondary" role="status">
+      <span class="sr-only">Loading...</span>
+    </div> 
+    <div class="spinner-grow text-success" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div class="spinner-grow text-danger" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div class="spinner-grow text-warning" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div class="spinner-grow text-info" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div class="spinner-grow text-light" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div class="spinner-grow text-dark" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    
+    
+    <ul class="list-unstyled">
+      <li class="media">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD1SM1m9tVtfvpF94RJr_HbrBfOq1hpmuIbIMmk1C7ugLl8PF8" class="mr-3" alt="...">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">Programming</h5>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione exercitationem esse quia, sed, nobis tempore vitae non, sapiente cum nam culpa et impedit vero ipsum accusantium! Maiores praesentium asperiores ducimus?
+        </div>
+      </li>          
+    </ul>
+    
+    <div class="jumbotron">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" style="background:#6f42c1" href="#" role="button">Learn more</a>
+    </div>          
+  </div>
+</template>
---
+++ script.js
@@ -0,0 +1,16 @@
+import home from './components/home.vue'
+import pa from './components/pa.vue'
+
+
+
+
+
+new Vue({
+  el: '#app',
+  components: {
+    home,
+    pa,
+    
+  }
+
+})
